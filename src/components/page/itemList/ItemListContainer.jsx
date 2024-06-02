2e054e0a3daf0df99830b1d623bb918561059ed8
Marcelo Lamaruggine <marcelolama.27@gmail.com>
2024-06-02 20:54:43 -0300

    utilizamos Axios para obtener los productos desde la api FakeStoreApi

--- src/components/page/itemList/ItemListContainer.jsx
+++ src/components/page/itemList/ItemListContainer.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
+import axios from "axios";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products") //Primer promesa
-      .then((resp) => resp.json())             // El resp.json() devuelve otra promesa
-      .then((data) => setItems(data))
-      .catch((error) => console.log(error) );
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((resp) => setItems(resp.data))
+      .catch((error) => console.log("Error de conexion:", error));
   }, []);
 
   return <ItemList items={items} />;
